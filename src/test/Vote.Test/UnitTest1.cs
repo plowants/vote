8f21a50066b539835f06480dca38e5e54cb06a62
Liu.M <lxf276541049@163.com>
2019-11-26 16:05:08 +0800

    添加单元测试项目

---
+++ src/test/Vote.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vote.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string str="1";
+            if(str!="2"){
+                
+            }
+        }
+    }
+}
